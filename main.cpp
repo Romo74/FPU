91c6d9fb57bf1f1767709bba704a899d767b3ce2
carlos <carlosernestoone@gmail.com>
2022-02-27 16:47:52 -0600

    ss

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string codify(float number){
+    return "";
+}
+float decode(const string& ieee_754){
+    return 0;
+}
+
+float addition(string n1,string n2){
+    return decode(n1)+ decode(n2);
+}
+float multiplication(string n1, string n2){
+    return decode(n1)* decode(n2);
+}
