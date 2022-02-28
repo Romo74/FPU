974bcdcbf8fa0a5270a9b085db5436f802111d3b
Romo74 <100547129+Romo74@users.noreply.github.com>
2022-02-27 20:52:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,3 +15,8 @@ float addition(string n1,string n2){
 float multiplication(string n1, string n2){
     return decode(n1)* decode(n2);
 }
+float sqrtt(string n1){
+    float i;
+    for(i=0.01;i*i<decode(n1);i=i+0.01);
+    return(printf("%.2f", i));
+}
